e63c56fe84ad5991b4ccee70555395f16efbe708
Peng Liu <myme5261314@gmail.com>
2014-10-07 08:58:08 +0800

    Try to run some experiments to find out the speedup ratio.

--- src/dbn/testDBN.java
+++ src/dbn/testDBN.java
@@ -3,6 +3,8 @@
  */
 package dbn;
 
+import java.util.Arrays;
+
 import nn.NN;
 
 import org.jblas.DoubleMatrix;
@@ -28,19 +30,27 @@ public class testDBN {
 		getXY(xy);
 		long endtime = System.currentTimeMillis();
 		dispTime("Finish Load Data. ", begintime, endtime);
-		begintime = System.currentTimeMillis();
 		Option op = new Option();
-		op.numepochs = 10;
+		op.numepochs = 30;
 		op.momentum = 0;
 		op.alpha = 1;
-		op.push = 5;
-		op.fetch = 10;
+		op.push = 10;
+		op.fetch = 20;
 		int[] size = { 100, 100 };
-//		DBN dbn = new DBN(xy[0], op, size);
-//		dbn.setup();
-//		dbn.train();
-//		endtime = System.currentTimeMillis();
-//		dispTime("Finish normal RBM. ", begintime, endtime);
+		int repeat = 10;
+		int thread = 20;
+		double nRBM_time = 0;
+		double[] aRBM_time = new double[thread-1];
+		for (int i = 0; i < repeat; i++) {
+			DBN dbn = new DBN(xy[0], op, size);
+			dbn.setup();
+			begintime = System.currentTimeMillis();
+			dbn.train();
+			endtime = System.currentTimeMillis();
+			dispTime("Finish normal RBM. ", begintime, endtime);
+			nRBM_time += (endtime-begintime)/1000/repeat;
+		}
+
 //		begintime = System.currentTimeMillis();
 //		NN nn = new NN(dbn, 10);
 //		nn.train(xy[0], xy[1], op);
@@ -50,22 +60,30 @@ public class testDBN {
 //		System.out.println("The accuracy on test set is " + err);
 //		endtime = System.currentTimeMillis();
 //		dispTime("Finish normalRBM's NN. ", begintime, endtime);
-
-		begintime = System.currentTimeMillis();
-		AsyDBN asy_dbn = new AsyDBN(xy[0], op, size, 4);
-		asy_dbn.setup();
-		asy_dbn.train();
-		endtime = System.currentTimeMillis();
-		dispTime("Finish Asynchronous RBM. ", begintime, endtime);
-		begintime = System.currentTimeMillis();
-		NN nn1 = new NN(asy_dbn, 10);
-		nn1.train(xy[0], xy[1], op);
-		double err1 = nn1.test(xy[0], xy[1]);
-		System.out.println("The accuracy on train set is " + err1);
-		err1 = nn1.test(xy[2], xy[3]);
-		System.out.println("The accuracy on test set is " + err1);
-		endtime = System.currentTimeMillis();
-		dispTime("Finish AsyRBM's NN. ", begintime, endtime);
+		
+		for (int i = 1; i < thread; i++) {
+			aRBM_time[i-1] = 0;
+			for (int j = 0; j < repeat; j++) {
+				AsyDBN asy_dbn = new AsyDBN(xy[0], op, size, i+1);
+				asy_dbn.setup();
+				begintime = System.currentTimeMillis();
+				asy_dbn.train();
+				endtime = System.currentTimeMillis();
+				dispTime("Finish Asynchronous RBM. ", begintime, endtime);
+				aRBM_time[i-1] += (endtime-begintime)/1000/repeat;
+			}
+		}
+		System.out.println(nRBM_time);
+		System.out.println(Arrays.toString(aRBM_time));
+//		begintime = System.currentTimeMillis();
+//		NN nn1 = new NN(asy_dbn, 10);
+//		nn1.train(xy[0], xy[1], op);
+//		double err1 = nn1.test(xy[0], xy[1]);
+//		System.out.println("The accuracy on train set is " + err1);
+//		err1 = nn1.test(xy[2], xy[3]);
+//		System.out.println("The accuracy on test set is " + err1);
+//		endtime = System.currentTimeMillis();
+//		dispTime("Finish AsyRBM's NN. ", begintime, endtime);
 
 	}
 
